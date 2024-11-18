9e935d2c0d0225a4e4a1a50271d37d9a531f364c
DylanMif <dylan.miftari@etu.univ-savoie.fr>
2024-11-18 16:44:07 +0100

    7-Binding

---
+++ Document App/DocumentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DocumentViewController.swift
+//  Document App
+//
+//  Created by Dylan MIFTARI on 11/18/24.
+//
+
+import UIKit
+
+class DocumentViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
